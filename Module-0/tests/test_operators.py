66c74c67bc00ade2b8d4c8934747330cd337427d
ben <wang.618.bin@gmail.com>
2021-12-14 15:51:12 -0500

    task0_2: property test

--- Module-0/tests/test_operators.py
+++ Module-0/tests/test_operators.py
@@ -105,48 +105,52 @@ def test_sigmoid(a):
     * It crosses 0 at 0.5
     * it is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert operators.sigmoid(a) >= 0.0 and operators.sigmoid(a) <= 1.0
+    assert_close(1 - operators.sigmoid(a), operators.sigmoid(-a))
+    assert_close(operators.sigmoid(0), 0.5)
+    if a > 0:
+        assert operators.sigmoid(-a) < 0.5 and operators.sigmoid(a) > 0.5
+    else:
+        assert operators.sigmoid(a) <= 0.5 and operators.sigmoid(-a) >= 0.5
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a, b, c):
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if operators.lt(a, b) and operators.lt(b, c):
+        assert operators.lt(a, c)
 
 
 @pytest.mark.task0_2
-def test_symmetric():
+@given(small_floats, small_floats)
+def test_symmetric(a, b):
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(operators.add(a, b), operators.add(b, a))
+    assert_close(operators.mul(a, b), operators.mul(b, a))
 
 
 @pytest.mark.task0_2
-def test_distribute():
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x, y, z):
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(operators.mul(z, operators.add(x, y)), operators.add(operators.mul(z, x), operators.mul(z, y)))
 
 
 @pytest.mark.task0_2
-def test_other():
+@given(small_floats)
+def test_other(a):
     """
     Write a test that ensures some other property holds for your functions.
     """
-    None
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert a == operators.neg(operators.neg(a))
+    assert_close(operators.sigmoid(-a), 1 - operators.sigmoid(a))
 
 
 # ## Task 0.3  - Higher-order functions
