ca8125983b77fc2fa9ade468271a83c6d1d40b57
ben <wang.618.bin@gmail.com>
2021-12-16 16:18:41 -0500

    task1.5: network

--- Module-1/project/run_scalar.py
+++ Module-1/project/run_scalar.py
@@ -9,8 +9,9 @@ import random
 class Network(minitorch.Module):
     def __init__(self, hidden_layers):
         super().__init__()
-        # TODO: Implement for Task 1.5.
-        raise NotImplementedError('Need to implement for Task 1.5')
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
 
     def forward(self, x):
         middle = [h.relu() for h in self.layer1.forward(x)]
@@ -39,8 +40,11 @@ class Linear(minitorch.Module):
             )
 
     def forward(self, inputs):
-        # TODO: Implement for Task 1.5.
-        raise NotImplementedError('Need to implement for Task 1.5')
+        y = [b.value for b in self.bias]
+        for j in range(len(y)):
+            for i, xi in enumerate(inputs):
+                y[j] += xi * self.weights[i][j].value
+        return y
 
 
 def default_log_fn(epoch, total_loss, correct, losses):
